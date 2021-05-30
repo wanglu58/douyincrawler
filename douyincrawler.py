cbe6a8bc2355ae51620ca972f4dc0c001e8c0ddc
wanglu <wanglu@enginetech.cn>
2021-05-30 16:03:36 +0800

    update

--- douyincrawler.py
+++ douyincrawler.py
@@ -4,9 +4,25 @@ import os
 import re
 import time
 import uuid
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import requests
 from dateutil.relativedelta import relativedelta
+
+
+def get_video(key, value):
+    with open(value, 'wb') as v:
+        try:
+            v.write(requests.get(url=key, headers=headers).content)
+            global total_num
+            total_num += 1
+            return f'{value.split("/")[-1][:-4]} ===> 下载成功。'
+        except Exception as e:
+            with open(f'{Path}/失败链接.txt', 'a', encoding='utf-8') as f:
+                f.write(key)
+                f.write('\n')
+            return f'{value.split("/")[-1][:-4]} ===> 下载失败。'
+
 
 awemeurl = 'https://www.iesdouyin.com/web/api/v2/aweme/post/?'
 douyinurl = 'https://www.iesdouyin.com/web/api/v2/user/info/?'
@@ -41,6 +57,8 @@ userinfo = json.loads(getname)
 name = userinfo['user_info']['nickname']
 Path = name
 
+print('\n获取视频链接中，请稍候。。。。。。\n')
+json_data = {}
 k = len(timepool)
 total_num = 0
 for i in range(k):
@@ -71,16 +89,15 @@ for i in range(k):
             for s in '\\/:*?\"<>|':
                 videotitle = videotitle.replace(s, '')
             videourl = data['aweme_list'][i]['video']['play_addr']['url_list'][0]
-            with open(f'{Path}/{time_str}-{awemenum}/{videotitle}.mp4', 'wb') as v:
-                try:
-                    v.write(requests.get(url=videourl, headers=headers).content)
-                    print(f'{videotitle} ===> 下载成功。')
-                    total_num += 1
-                except Exception as e:
-                    print(f'{videotitle} ===> 下载失败。')
-                    with open(f'{Path}/失败链接.txt', 'a', encoding='utf-8') as f:
-                        f.write(f'{videourl}')
-                        f.write('\n')
+            json_data[videourl] = f'{Path}/{time_str}-{awemenum}/{videotitle}.mp4'
+
+task_list = []
+with ThreadPoolExecutor() as executor:
+    for key, value in json_data.items():
+        task = executor.submit(get_video, key, value)
+        task_list.append(task)
+    for res in as_completed(task_list):
+        print(res.result())
 
 print(f'\n共下载 {total_num} 个抖音视频，请在当前路径下查看！')
 print('Enjoy it')
